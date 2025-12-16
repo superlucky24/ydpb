536875a9ac07e26ac5b7536a5c4a027147b0a381
rainwell79-web <rainwell79@gmail.com>
2025-12-16 17:10:32 +0900

    빈 링크 경고창 스크립트 추가

---
+++ js/ydpb.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    // 링크 # 처리한 a 태그 클릭 이벤트 시 경고창 출력 : 20251216 최상림 추가
+    $('a[href="#"]').click(function(e) {
+        e.preventDefault();
+        layerAlert('죄송합니다.<br> 현재 준비중인 메뉴입니다.');
+    });
+
+    // 액션 # 처리한 form 태그 submit 이벤트 시 경고창 출력 : 20251216 최상림 추가
+    $('form[action="#"]').submit(function(e) {
+        e.preventDefault();
+        layerAlert('죄송합니다.<br> 현재 준비중인 메뉴입니다.');
+    });
+
+});
+
+/**
+ * 레이어 경고창 생성 함수 : 20251216 최상림 추가
+ * @param {String} text : 경고창 내용
+ */
+function layerAlert(text) {
+    const fadeInTime = 600;
+    const fadeOutTime = 600;
+    const delayTime = 1000;
+
+    // 현재 경고창이 없을 때만 동작
+    if($('.layer_alert').length == 0) {
+        let html = '<div class="layer_alert">' + text + '</div>';
+        $('body').append(html);
+        $('.layer_alert').stop().fadeIn(fadeInTime);
+        setTimeout(function() {
+            $('.layer_alert').stop().fadeOut(fadeOutTime, function() {
+                $(this).remove();
+            });
+        }, delayTime);
+    }
+}
