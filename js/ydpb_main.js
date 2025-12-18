1a4855fd8173374c8600fc061aad39681ebc8d90
bill5680 <58128751+bill5680@users.noreply.github.com>
2025-12-18 16:04:26 +0900

    ydpd_index js 부분

---
+++ js/ydpb_main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    $('.side_list_menu>div').on('click', function(){        
+        $(this).parent('.side_list_menu').siblings().removeClass('open');
+        $(this).parent().siblings().find('.sub_list').removeClass('show');
+        $(this).parent().siblings().find('.sub_group').removeClass('show');
+        $(this).next('.sub_list').toggleClass('show');
+        $(this).next().find('.sub_group').toggleClass('show');
+        $(this).parent().toggleClass('open');
+    });
+
+    $('.sub_group_title').on('click', function(){
+
+        $(this).toggleClass('open');
+        $(this).next('.sub_group_list').toggleClass('show');
+    });
+
+    $('.main3_dong').on('click', function() {
+        $(this).toggleClass('checked');
+        $('.main3_contents').removeClass('show');
+        $('.m3dong').addClass('show');
+    });
+    $('.main3_gu').on('click', function() {
+        $(this).toggleClass('checked');
+        $('.main3_contents').removeClass('show');
+        $('.m3gu').addClass('show');
+    });
+
+    $('.loc_sns').on('click', function() {
+        $(this).toggleClass('checked');
+        $('.sns_list').toggleClass('show');
+    });
+
+});
